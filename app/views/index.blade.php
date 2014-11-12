b4c136d11a2df538b145cab89085d4e072f78df4
valde1992 <fernando_valde@hotmail.com>
2014-11-12 20:48:57 -0300

    Se edita el color de algunos textos

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -146,7 +146,7 @@
 						    <div class="modal-content">
 						      <div class="modal-header">
 						        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-						        <h4 class="modal-title" id="myModalLabel2"><center>{{ $comunidad->nombre }}</center></h4>
+						        <h4 class="modal-title text-primary" id="myModalLabel2" ><center>{{ $comunidad->nombre }}</center></h4>
 						      </div>
 						      <div class="modal-body">
 						      	<div class="container-fluid">
